eddf1399836df1d19e8f9e28660bcfd632296ee4
DiegoELT99 <diegoelt99@gmail.com>
2020-04-01 14:20:58 -0500

    Added build plugins to the pom file and switched to log4j

--- hand_wash_mvn/src/main/java/cs_utec/cs2901/App.java
+++ hand_wash_mvn/src/main/java/cs_utec/cs2901/App.java
@@ -1,14 +1,18 @@
 package cs_utec.cs2901;
 
+import org.apache.log4j.Logger; // For the updated homework
+
 /* Basic assignment in Java to teach us how to use mvn */
 public class App 
 {
+    final static Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
         String washing = "Me lavo las manos por 20 segundos con agua y con jabón"; 
         for (int a = 0; a < 100; a++)
         {
-            System.out.println(washing);
+            logger.info(washing); // Some other alternatives include debug, warn, error, and fatal. 
         }
     }
 }
