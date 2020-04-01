c4c95a1905ec137d1305548bcf0174178c2ab5ac
DiegoELT99 <diegoelt99@gmail.com>
2020-04-01 13:36:47 -0500

    Hand wash classwork

---
+++ hand_wash_mvn/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,14 @@
+package cs_utec.cs2901;
+
+/* Basic assignment in Java to teach us how to use mvn */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String washing = "Me lavo las manos por 20 segundos con agua y con jabón"; 
+        for (int a = 0; a < 100; a++)
+        {
+            System.out.println(washing);
+        }
+    }
+}
